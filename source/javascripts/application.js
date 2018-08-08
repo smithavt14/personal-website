ef4a3a056f7155c5077dc5223e3277f9d3148a6e
Alex Smith <smithavt14@gmail.com>
2018-08-08 11:03:21 +0800

    Made the page more dynamic, created the second section (experience) and started on the third section

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -2,13 +2,3 @@
 //= require bootstrap
 //= require_tree .
 //= require typed.js
-
-function loadDynamicBannerText() {
-  new Typed('#banner-typed-text', {
-    strings: ["Change your life", "Learn to code"],
-    typeSpeed: 50,
-    loop: true
-  });
-}
-
-loadDynamicBannerText();
